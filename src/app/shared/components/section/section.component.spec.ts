3888c3cbb68eb864b2c902558be7a6dda96c9ae9
Meet Shah <meetdshah007@gmail.com>
2020-07-06 16:17:24 +0530

    Added unit testing for section

--- src/app/shared/components/section/section.component.spec.ts
+++ src/app/shared/components/section/section.component.spec.ts
@@ -1,3 +1,4 @@
+import { By } from '@angular/platform-browser';
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { SectionComponent } from './section.component';
@@ -6,29 +7,37 @@ import { RetroBoardImports } from '../../../testing/retro-board-imports';
 
 describe('SectionComponent', () => {
   let component: SectionComponent;
+  let componentEl: HTMLElement;
   let fixture: ComponentFixture<SectionComponent>;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ SectionComponent, CardComponent ],
+      declarations: [SectionComponent, CardComponent],
       imports: [
         ...RetroBoardImports
       ]
     })
-    .compileComponents();
-  }));
-
-  beforeEach(() => {
+      .compileComponents();
     fixture = TestBed.createComponent(SectionComponent);
-    component = fixture.componentInstance;
+    component = fixture.debugElement.componentInstance;
+    componentEl = fixture.debugElement.nativeElement;
     fixture.detectChanges();
-  });
+  }));
 
   it('should create', () => {
     expect(component).toBeTruthy();
   });
 
-  it('should add card on mocking onAdd', ()=>{
+  it('should render Add link & title', () => {
+    expect(componentEl.querySelector('h5').textContent).toBe('');
+    expect(componentEl.querySelector('button.btn-link').textContent).toBe('+ Add');
+  });
 
-  })
+  it('should display modal on clicking the Add link', () => {
+    const spier = spyOn(component, 'showModal');
+    const addBtnEl = fixture.debugElement.query(By.css('button.btn-link'));
+    addBtnEl.triggerEventHandler('click', null);
+    fixture.detectChanges();
+    expect(spier).toHaveBeenCalled();
+  });
 });
